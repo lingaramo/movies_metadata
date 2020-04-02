913bf09616049662c8fd3a3333c7a5411edd735b
Lucas Ingaramo <l.ingaramo@gmail.com>
2020-04-02 11:41:55 -0300

    Create genres table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_143344) do
+ActiveRecord::Schema.define(version: 2020_04_02_144040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.index ["name"], name: "index_genres_on_name", unique: true
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "name", null: false
